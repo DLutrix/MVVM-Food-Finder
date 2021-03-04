47d951f2e886b8c22531ad1e022e19f26c2caf9b
w0rm1995 <meld.mee@gmail.com>
2021-03-04 18:26:16 +0800

    Change delivery and takeway type to Any

--- app/src/main/java/com/dlutrix/foodfinder/data/model/HasMenuStatus.kt
+++ app/src/main/java/com/dlutrix/foodfinder/data/model/HasMenuStatus.kt
@@ -4,6 +4,7 @@ package com.dlutrix.foodfinder.data.model
 import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
+import kotlinx.android.parcel.RawValue
 
 /**
  * w0rm1995 on 16/10/20.
@@ -11,6 +12,6 @@ import kotlinx.android.parcel.Parcelize
  */
 @Parcelize
 data class HasMenuStatus(
-    val delivery: Int,
-    val takeaway: Int
+    val delivery: @RawValue Any,
+    val takeaway: @RawValue Any
 ) : Parcelable
